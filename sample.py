254e9d14bca3e1e2444be372cc5bfad9dc889dce
akai-tsuki <akai.tsuki.mobile@gmail.com>
2016-04-30 21:20:36 +0900

    change output to a file.

--- sample.py
+++ sample.py
@@ -5,21 +5,9 @@ import subprocess
 import logging
 import logging.config
 
-logging.config.fileConfig('logging.conf')
-
-#logger = logging.getLogger(__name__)
+logging.config.fileConfig("/home/work/ping-tool/logging.conf")
 logger = logging.getLogger("pingApp")
-#logger.setLevel(logging.DEBUG)
 
-#handler = logging.StreamHandler()
-#handler.setLevel(logging.DEBUG)
-
-#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-#handler.setFormatter(formatter)
-
-#logger.addHandler(handler)
-
-# host = '172.17.0.1'
 hosts = ['172.17.0.1', '172.17.0.2', '192.168.99.100']
 
 for host in hosts:
@@ -35,3 +23,4 @@ for host in hosts:
   
   for line in res.splitlines():
     logger.info(line)
+
