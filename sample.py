9a356661b8c86326ab613a027e2c8d861ae76a8f
akai-tsuki <akai.tsuki.mobile@gmail.com>
2016-04-30 20:05:52 +0900

    add logging formatter

--- sample.py
+++ sample.py
@@ -10,13 +10,16 @@ logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
 
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
 logger.addHandler(handler)
 
 # host = '172.17.0.1'
 hosts = ['172.17.0.1', '172.17.0.2', '192.168.99.100']
 
 for host in hosts:
-  args = ['ping', '-c 3', host]
+  args = ['ping', '-c 1', host]
 
   try:
   #  res = subprocess.check_call(args)
