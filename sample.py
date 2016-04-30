7c96e34cc70b681fe68863eeb3b5006eac5b5802
akai-tsuki <akai.tsuki.mobile@gmail.com>
2016-04-30 19:03:09 +0900

    add roop

--- sample.py
+++ sample.py
@@ -3,15 +3,16 @@
 
 import subprocess
 
-# 192.168.99.100'
-host = '172.17.0.1'
-args = ['ping', '-c 3', host]
+# host = '172.17.0.1'
+hosts = ['172.17.0.1', '172.17.0.2', '192.168.99.100']
 
-try:
-#  res1 = subprocess.check_call(args)
-  res = subprocess.check_output(args)
-except:
-  res = "Error. (" + host + ")"
+for host in hosts:
+  args = ['ping', '-c 3', host]
 
-#print res1
-print res
+  try:
+  #  res = subprocess.check_call(args)
+    res = subprocess.check_output(args)
+  except:
+    res = "Error. (" + host + ")"
+  
+  print res
