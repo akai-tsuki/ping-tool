0f11407245169b2de1c910af38fb18e4dafdfb79
akai-tsuki <akai.tsuki.mobile@gmail.com>
2016-05-01 16:07:13 +0000

    add None check

--- sample.py
+++ sample.py
@@ -14,6 +14,7 @@ parser = argparse.ArgumentParser(description='This is tool sample.')
 parser.add_argument('-t', '--tool', type=str, help='tool name')
 args = parser.parse_args()
 
+
 logging.config.fileConfig("./conf/logging.conf")
 logger = logging.getLogger("pingApp")
 console_out = logging.getLogger("console")
@@ -21,6 +22,9 @@ console_out = logging.getLogger("console")
 start_time = today.strftime("%Y/%m/%d %H:%M:%S")
 console_out.info("start : " + start_time)
 
+
+if args.tool is not None:
+  console_out.info("tool : " + args.tool)
 
 logger.debug('ping check : start')
 
